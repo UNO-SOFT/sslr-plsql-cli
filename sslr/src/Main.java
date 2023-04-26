245bca8fad7a9e34458f74a55d1b789e3a88c67d
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2023-04-26 09:22:39 +0200

    stdin/out to XML works

--- sslr/src/Main.java
+++ sslr/src/Main.java
@@ -2,28 +2,42 @@ package sslr;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 
 import com.A.B.H.C;
 import com.sonar.oracle.H;
+import com.sonar.oracle.toolkit.A;
+import com.sonar.sslr.api.AstNode;
+import com.sonar.sslr.impl.ast.AstXmlPrinter;
+import org.sonar.B.E;
+import org.sonar.sslr.grammar.LexerlessGrammarBuilder;
+import org.sonar.sslr.internal.toolkit.SourceCodeModel;
 import org.sonar.sslr.parser.ParseRunner;
+import org.sonar.sslr.parser.ParserAdapter;
 import org.sonar.sslr.parser.ParsingResult;
 import com.sonar.sslr.impl.Parser;
-import org.sonar.sslr.toolkit.ConfigurationProperty;
-import org.sonar.sslr.toolkit.Validators;
+import org.sonar.sslr.toolkit.*;
 
 public class Main {
     ParseRunner parseRunner;
     @C
     protected H B() {
         return com.sonar.oracle.H.B().A(
-                Charset.forName("UTF-8")
-        ).A(true).D();
+                StandardCharsets.UTF_8
+        ).A(false).D();
     }
 
     public Main() {
+        LexerlessGrammarBuilder builder = com.sonar.oracle.A.B();
+        H h = this.B();
+        /*
         this.parseRunner = new ParseRunner(
-                com.sonar.oracle.I.A(this.B()).getRootRule()
-        );
+                com.sonar.oracle.I.A(
+                        h,
+                        (com.sonar.sslr.impl.Parser)(new org.sonar.sslr.parser.ParserAdapter(h.C(), builder.build())))
+        );*/
     }
 
     public ParsingResult parse(char[] data ) {
@@ -31,18 +45,24 @@ public class Main {
     }
 
     public static void main(final String[] args) {
+        ConfigurationModel cm = new com.sonar.oracle.toolkit.A();
+        org.sonar.sslr.toolkit.Toolkit tk = new org.sonar.sslr.toolkit.Toolkit("SSLR :: PL/SQL :: Toolkit", cm);
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char []data = null;
         try {
-            CharArrayWriter caw = new CharArrayWriter(1024);
-            br.transferTo(caw);
-            data = caw.toCharArray();
+            StringWriter sw = new StringWriter(1024);
+            br.transferTo(sw);
+            String s = sw.toString();
+
+            AstNode node = cm.getParser().parse(s);
+
+            System.out.println("node=" + node);
+
+            OutputStreamWriter osw = new OutputStreamWriter(System.out);
+            AstXmlPrinter.print(node, osw);
+            osw.flush();
         } catch(IOException ioe) {
             System.out.println(ioe);
         }
-        Main m = new Main();
-
-        ParsingResult res = m.parse(data);
-        System.out.println("res=" + res);
     }
 }
